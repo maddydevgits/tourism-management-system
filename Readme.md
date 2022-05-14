8ce3d90b682b34cb36df7875b7cf29ea97c779e5
madblocksgit <madblocks2k19@gmail.com>
2022-05-14 11:19:41 +0530

    created basic html for user to interact

--- Readme.md
+++ Readme.md
@@ -2,4 +2,5 @@
 Tourism Management System is a recommender application for tourists about places to visit, hotels to say and restaraunts to have hangout with family and friends.
 
 # Development Process
-1. Create a basic html for user to interact with chatbot :x:
+1. Create a basic html for user to interact with chatbot :heavy_check_mark:
+2. Create recommendations for tourist spot :x:
--- app.py
+++ app.py
@@ -5,22 +5,54 @@ from dataset import *
 msgId=0
 
 def chatbot_response(msg):
+    global msgId
     res=''
     if int(msg)==1 and msgId==0:
         msgId=1
-        res+='Here are some Top Rate places to visit: <br/>'
+        res+='<b>Here are some Top Rate places to visit:</b> <br/>'
         for i in random.sample(Lakes,2):
             res+=i+'<br/>'
-        res+='<br/><br/>'
+        res+='<br/>'
         for i in random.sample(Sites,1):
             res+=i+'<br/>'
-        res+='<br/><br/>'
+        res+='<br/>'
         for i in random.sample(Historical,3):
             res+=i+'<br/>'
-        res+='<br/><br/>'
-        res+=Seasonal[1]    
+        res+='<br/>'
+        res+=Seasonal[1]+'<br/>'    
         res+='<br/><br/>'
         res+='If you are looking for something specific, Please select: <br/> 1. Lakes <br/>2. Site Seeing <br/>3. Historical Places<br/>4. Seasonal Spots<br/>5. Main menu<br/>6. Say Goodbye <br/>'
+    elif((msgId==1) and int(msg)==1):
+        res+='<b>Here is the list of Top 7 Lakes in the City</b> <br/>'
+        for i in range(len(Lakes)):
+            res+=Lakes[i]+'<br/>'
+        res+='<br/><br/>'
+        res+='If you are looking for something specific, Please select: <br/> 1. Lakes <br/>2. Site Seeing <br/>3. Historical Places<br/>4. Seasonal Spots<br/>5. Main menu<br/>6. Say Goodbye <br/>'
+    elif((msgId==1) and (int(msg)==2)):
+        res+='<b>Here is the list of Top 3 Site Seeing Places in the City</b> <br/>'
+        for i in range(len(Sites)):
+            res+=Sites[i]+'<br/>'
+        res+='<br/><br/>'
+        res+='If you are looking for something specific, Please select: <br/> 1. Lakes <br/>2. Site Seeing <br/>3. Historical Places<br/>4. Seasonal Spots<br/>5. Main menu<br/>6. Say Goodbye <br/>'
+    elif((msgId==1) and int(msg)==3):
+        res+='<b>Here is the lis of Top 14 Historical Places of the City</b> <br/>'
+        for i in range(len(Historical)):
+            res+=Historical[i]+'<br/>'
+        res+='<br/><br/>'
+        res+='If you are looking for something specific, Please select: <br/> 1. Lakes <br/>2. Site Seeing <br/>3. Historical Places<br/>4. Seasonal Spots<br/>5. Main menu<br/>6. Say Goodbye <br/>'
+    elif((msgId==1) and int(msg)==4):
+        res+='<b>Here is list of some Famous Seasonal Fests of the City</b> <br/>'
+        for i in range(len(Seasonal)):
+            res+=Seasonal[i]+'<br/>'
+        res+='<br/><br/>'
+        res+='If you are looking for something specific, Please select: <br/> 1. Lakes <br/>2. Site Seeing <br/>3. Historical Places<br/>4. Seasonal Spots<br/>5. Main menu<br/>6. Say Goodbye <br/>'
+    elif((msgId==1) and int(msg)==5):
+        res+='Tell me What Can I do for you? <br/>1. Recommend a Tourist Spot <br/>2. Recommend some good Restaurants <br/>3. Tell some Good Hotels <br/>4. Checkout our Exclusive Packages <br/>5. Exit'
+        msgId=0
+    elif((msgId==1) and int(msg)==6):
+        res+='Time to say Goodbye, Hope to see you soon <br/>Happy Travelling'
+        msgId=0
+
     return res
 
 app=Flask(__name__)
